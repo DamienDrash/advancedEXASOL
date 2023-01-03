f3da27b601e2c85b2100dede1f47eb3fb89b035a
Damien Frigewski <damien.frigewski@aaa-internetshops.de>
2023-01-03 15:06:19 +0100

    Bug fixing pypi

--- build/lib/advancedEXASOL/advanced_exasol.py
+++ build/lib/advancedEXASOL/advanced_exasol.py
@@ -1,4 +1,4 @@
-from decorator import ensure_table_exists, ensure_connection, handle_transactions, sql_injection_safe, enforce_resource_limits
+from .decorator import ensure_table_exists, ensure_connection, handle_transactions, sql_injection_safe, enforce_resource_limits
 import pyexasol
 import dask.dataframe as dd
 import pandas as pd
